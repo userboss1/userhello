68efeafee90b2792513fa05a424cb8e83b4beeea
userboss1 <carsadhil@gmail.com>
2025-03-06 19:44:11 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,38 +1,44 @@
 var express = require('express');
 var router = express.Router();
-var userHelper=require('../helper/user-helper');
-
+var userHelper = require('../helper/user-helper');
 
 /* GET home page. */
-
-router.get('/', function(req, res, next) {
-let user =req.session.user
-  res.render('user/intro',{user,user:true});
-  
-
-
- 
+router.get('/', function (req, res, next) {
+  let user = req.session.user || null;  // Ensuring `user` is always defined
+  res.render('user/intro', { user, user: true });
 });
-router.get('/SignIn',(req,res)=>{
-  res.render('user/signup')
-})
-router.post('/signUphbs', (req,res)=>{
 
-  
- userHelper.doSignup(req.body).then((response)=>{
-   req.session.user=response
+/* User Signup */
+router.get('/SignIn', (req, res) => {
+  res.render('user/signup');
+});
 
-   
-res.redirect('/')
-  })
-})
-router.get('/LogOut',(req,res)=>{
-  console.log("clicked")
-  req.session.destroy()
-  res.redirect('/')
-})
-router.get('/logIn',(req,res)=>{
-  res.render('admin/login')
-})
+router.post('/signUp', (req, res) => {  // Renamed to `signUp` for clarity
+  userHelper.doSignup(req.body)
+    .then((response) => {
+      req.session.user = response;
+      res.redirect('/');
+    })
+    .catch((err) => {
+      console.error("Signup Error:", err);
+      res.redirect('/SignIn');
+    });
+});
+
+/* User Logout */
+router.get('/LogOut', (req, res) => {
+  console.log("Logout clicked");
+  req.session.destroy((err) => {
+    if (err) {
+      console.error("Session destruction error:", err);
+    }
+    res.redirect('/');
+  });
+});
+
+/* User Login */
+router.get('/logIn', (req, res) => {
+  res.render('user/login');  // Fixed the incorrect admin login page
+});
 
 module.exports = router;
