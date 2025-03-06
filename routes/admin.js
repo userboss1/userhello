57e62473c2ac2050f3acb87693ae10f844cd5eb5
userboss1 <carsadhil@gmail.com>
2025-03-06 19:41:58 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,120 +1,153 @@
 var express = require('express');
 const adminHelper = require('../helper/admin-product-helper');
-const  adminLogin  = require('../helper/admin-login-user');
+const adminLogin = require('../helper/admin-login-user');
 
 var router = express.Router();
-const verifyLogin=(req,res,next)=>{
-  if(req.session.adminLog){
-    next()
+
+const verifyLogin = (req, res, next) => {
+  if (req.session.adminLog) {
+    next();
+  } else {
+    res.redirect('/logIn');
   }
-  else{
-    res.redirect('/logIn')
-  }
-}
-/* GET users listing. */
-router.get('/',verifyLogin,function(req, res, next) {
-  let admind = {
-    id: req.session.admin._id,
-    name: req.session.admin.name
 };
 
-
-    adminHelper.GetAllStock(admind).then((stock)=>{
-      
-      
-       
-      res.render('admin/mainpage',{admin:true,stock})
-    })})
-  
-
- 
-
-
-
-router.get('/signIn',(req,res)=>{
-  res.render('admin/signup')
-  })
-  router.post('/signIn',(req,res)=>{
-    adminLogin.adminSignUp(req.body).then((response)=>{
-
-      
-      req.session.adminLog=true;
-      req.session.admin=response
-      
-      res.redirect('/admin')
-    })
-    
-  })
-router.get('/AddProduct',verifyLogin,(req,res)=>{
-res.render('admin/addstock',{noheader:true})
-})
-router.post('/addstock',(req,res)=>{
-  console.log(req.body);
-  
- adminHelper.addStock(req.body,req.session.admin).then((response)=>{
- 
-  res.redirect('/admin')
- })
-})
-// router.get('/addQuantity/:id',(req,res)=>{
-//   let proId=req.params.id
-//   adminHelper.decrementStock(proId).then((response)=>{
-//   console.log("suces")
-
-//   })
-  
-// })
-router.get('/delete/:id',verifyLogin,(req,res)=>{
- let proId=req.params.id
- adminHelper.DeleteProduct(proId).then((response)=>{
-  console.log(response)
-  res.redirect("/admin")
- })
-  
-})
-router.get('/edit-product/:id',verifyLogin,async(req,res)=>{
-let product=await adminHelper.getStockDetails(req.params.id)
-
-res.render('admin/edit',{bodyR:true,product})
-})
-router.post('/editstock/:id',(req,res)=>{
-adminHelper.updateStockDetails(req.params.id,req.body).then(()=>{
-  res.redirect('/admin')
-})
-    
-})
-router.get('/userc',verifyLogin,(req,res)=>{
-  adminLogin.GetUserDetail().then((userLog)=>{
-    console.log(userLog);
-    
-   
-    res.render('admin/userList',{admin:true,userLog})
-    
-  })
-
-  
-})
-
-router.post('/logIn',(req,res)=>{
-adminLogin.adminLogin(req.body).then((response)=>{
-  if(response.status){
-    req.session.adminLog=true;
-    req.session.admin=response.user
-    res.redirect('/admin')
+/* GET Admin Main Page */
+router.get('/', verifyLogin, function (req, res, next) {
+  if (!req.session.admin) {
+    return res.redirect('/logIn');
   }
 
-})
-  
-    
-})
-router.get('/adduser',(req,res)=>{
-  res.render('admin/adduser')
-})
-router.post('/adduser',(req,res)=>{
-  adminLogin.AddUser(req.body).then(()=>{
-    res.redirect('/admin/userc',)
-  })
+  let admind = {
+    id: req.session.admin._id,
+    name: req.session.admin.name,
+  };
 
+  adminHelper.GetAllStock(admind)
+    .then((stock) => {
+      res.render('admin/mainpage', { admin: true, stock });
+    })
+    .catch((err) => {
+      console.error("Error fetching stock:", err);
+      res.redirect('/admin');
+    });
+});
 
-})
+/* Admin Sign Up */
+router.get('/signIn', (req, res) => {
+  res.render('admin/signup');
+});
+
+router.post('/signIn', (req, res) => {
+  adminLogin.adminSignUp(req.body)
+    .then((response) => {
+      req.session.adminLog = true;
+      req.session.admin = response;
+      res.redirect('/admin');
+    })
+    .catch((err) => {
+      console.error("Signup Error:", err);
+      res.redirect('/signIn');
+    });
+});
+
+/* Add Product */
+router.get('/AddProduct', verifyLogin, (req, res) => {
+  res.render('admin/addstock', { noheader: true });
+});
+
+router.post('/addstock', verifyLogin, (req, res) => {
+  console.log(req.body);
+
+  adminHelper.addStock(req.body, req.session.admin)
+    .then(() => {
+      res.redirect('/admin');
+    })
+    .catch((err) => {
+      console.error("Error adding stock:", err);
+      res.redirect('/admin');
+    });
+});
+
+/* Delete Product */
+router.get('/delete/:id', verifyLogin, (req, res) => {
+  let proId = req.params.id;
+  adminHelper.DeleteProduct(proId)
+    .then(() => {
+      res.redirect("/admin");
+    })
+    .catch((err) => {
+      console.error("Error deleting product:", err);
+      res.redirect('/admin');
+    });
+});
+
+/* Edit Product */
+router.get('/edit-product/:id', verifyLogin, async (req, res) => {
+  try {
+    let product = await adminHelper.getStockDetails(req.params.id);
+    res.render('admin/edit', { bodyR: true, product });
+  } catch (err) {
+    console.error("Error fetching product details:", err);
+    res.redirect('/admin');
+  }
+});
+
+router.post('/editstock/:id', verifyLogin, (req, res) => {
+  adminHelper.updateStockDetails(req.params.id, req.body)
+    .then(() => {
+      res.redirect('/admin');
+    })
+    .catch((err) => {
+      console.error("Error updating stock:", err);
+      res.redirect('/admin');
+    });
+});
+
+/* User Management */
+router.get('/userc', verifyLogin, (req, res) => {
+  adminLogin.GetUserDetail()
+    .then((userLog) => {
+      res.render('admin/userList', { admin: true, userLog });
+    })
+    .catch((err) => {
+      console.error("Error fetching user details:", err);
+      res.redirect('/admin');
+    });
+});
+
+/* Admin Login */
+router.post('/logIn', (req, res) => {
+  adminLogin.adminLogin(req.body)
+    .then((response) => {
+      if (response.status) {
+        req.session.adminLog = true;
+        req.session.admin = response.user;
+        res.redirect('/admin');
+      } else {
+        res.render('admin/login', { error: "Invalid credentials" });
+      }
+    })
+    .catch((err) => {
+      console.error("Login Error:", err);
+      res.redirect('/logIn');
+    });
+});
+
+/* Add User */
+router.get('/adduser', verifyLogin, (req, res) => {
+  res.render('admin/adduser');
+});
+
+router.post('/adduser', verifyLogin, (req, res) => {
+  adminLogin.AddUser(req.body)
+    .then(() => {
+      res.redirect('/admin/userc');
+    })
+    .catch((err) => {
+      console.error("Error adding user:", err);
+      res.redirect('/admin/userc');
+    });
+});
+
 module.exports = router;
